3e6fd6507bc11fc9aaf7dadb306d154a885356f4
Jawan0786 <166049823+Jawan0786@users.noreply.github.com>
2024-04-05 14:41:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -132,9 +132,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://g-one2021-bots.onrender.com/".format(FQDN)
+    URL = "https://mytestvj-1712ee43580d.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://g-one2021-bots.onrender.com/".format(FQDN)
+    URL = "https://mytestvj-1712ee43580d.herokuapp.com/".format(FQDN)
 
 
 
